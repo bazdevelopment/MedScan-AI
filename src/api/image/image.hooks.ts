87c90d9dd2546f569adbef94cc62390d4eb7abe1
Baziluc <bazilucmarian98@yahoo.com>
2024-10-31 09:27:52 +0200

    #55-removed logs

--- src/api/image/image.hooks.ts
+++ src/api/image/image.hooks.ts
@@ -1,26 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 import { type AxiosError } from 'axios';
-import { showMessage } from 'react-native-flash-message';
 
 import { analyzeImageUsingAi, analyzeVideoUsingAi } from './image.requests';
-
-interface IUseAnalyzeImageVariables {
-  userId: string;
-  base64Image: string;
-  imageType: string;
-}
 
 type Response = any;
 
 export const useAnalyzeImage = ({ onSuccessCallback }) =>
-  useMutation<Response, AxiosError, IUseAnalyzeImageVariables>({
+  useMutation<Response, AxiosError, FormData>({
     mutationFn: (variables) => analyzeImageUsingAi(variables),
     onSuccess: (data) => {
-      onSuccessCallback({ interpretationResult: data.interpretationResult });
+      !!data?.interpretationResult &&
+        onSuccessCallback({ interpretationResult: data.interpretationResult });
     },
-    onError: (error) => {
-      showMessage({ message: error });
-    },
+    onError: () => {},
   });
 
 export const useAnalyzeVideo = ({ onSuccessCallback }) =>

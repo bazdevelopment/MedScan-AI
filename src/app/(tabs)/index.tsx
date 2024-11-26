a966d0d3946b172a0053f194cbfb6bbf55d412e3
Baziluc <bazilucmarian98@yahoo.com>
2024-11-26 20:05:07 +0200

    #85-created custom stories for different scan categories

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,13 +5,14 @@ import { checkForAppUpdate } from 'firebase/remote-config';
 import React, { useEffect } from 'react';
 import { useStickyHeaderScrollProps } from 'react-native-sticky-parallax-header';
 
-import { useSendGlobalPushNotifications } from '@/api/push-notifications/push-notifications.hooks';
+import { useScanCategories } from '@/api/scan-categories/scan-categories.hooks';
 import { Foreground } from '@/components/home-foreground';
 import { HomeHeaderBar } from '@/components/home-header-bar';
 import ParallaxScrollView from '@/components/parallax-scrollview';
 import ReportCard from '@/components/report-card';
+import ScanCategoriesStories from '@/components/scan-category-stories';
 import { usePushNotificationSetup } from '@/core/hooks/use-push-notifications-setup';
-import { Button, ScrollView, Text, View } from '@/ui';
+import { ScrollView, Text, View } from '@/ui';
 
 const PARALLAX_HEIGHT = 310;
 const HEADER_BAR_HEIGHT = 110;
@@ -21,9 +22,10 @@ const SNAP_STOP_THRESHOLD = 330;
 export default function Home() {
   const { arePushNotificationEnabled, enablePushNotifications } =
     usePushNotificationSetup(); //todo: check if here is the best place to call the hook
-  // const { data } = useScanCategories();
-  const { mutate: onHandleGlobalPushNotifications } =
-    useSendGlobalPushNotifications();
+
+  // const { mutate: onHandleGlobalPushNotifications } =
+  //   useSendGlobalPushNotifications();
+  const { data, isPending: areScanCategoriesLoading } = useScanCategories();
 
   checkForAppUpdate();
 
@@ -64,7 +66,14 @@ export default function Home() {
     >
       <View>
         <View className="ml-4 mt-14">
-          <Text className="my-5">Recent reports</Text>
+          <Text className="my-6">Scan examples</Text>
+
+          <ScanCategoriesStories
+            categories={data?.categories}
+            isLoading={areScanCategoriesLoading}
+          />
+          <Text className="my-6">Recent reports</Text>
+
           <ScrollView
             horizontal
             showsHorizontalScrollIndicator={false}
@@ -84,8 +93,7 @@ export default function Home() {
             ))}
           </ScrollView>
 
-          <Text>{MOCK_TEXT}</Text>
-          <Button label="Send device token" onPress={enablePushNotifications} />
+          {/* <Button label="Send device token" onPress={enablePushNotifications} />
           <Button
             label="Submit notification"
             onPress={() =>
@@ -94,18 +102,9 @@ export default function Home() {
                 body: 'This is a global notification body',
               })
             }
-          />
+          /> */}
         </View>
       </View>
     </ParallaxScrollView>
   );
 }
-
-export const MOCK_TEXT = `
-Lorem ipsum dolor sit amet,  acilisi. Vestibulum tempor varius dolor, non condimentum lorem lacinia eu. Suspendisse dictum luctus facilisis. Suspendisse vehicula eget justo vitae viverra. Aenean eu augue quis massa faucibus auctor et ut metus. Praesent tincidunt risus ut ex convallis, non aliquam tortor euismod. Vestibulum congue, lorem vel vulputate tempus, enim magna eleifend orci, id semper diam orci at lorem. Curabitur eget metus faucibus neque facilisis scelerisque.
-
-Proin in mi scelerisque, luctus ante vel, rutrum odio. Curabitur placerat sit amet dui in fringilla. Morbi bibendum feugiat ultrices. Mauris pharetra ullamcorper ante vel semper. Mauris pulvinar, purus non egestas ornare, nibh purus vestibulum enim, nec feugiat nibh orci non ante. Praesent elementum mattis elit, eget facilisis dolor. Ut eget posuere lacus, nec ultricies dolor. Nunc a nunc eu felis dictum molestie. Etiam bibendum, dolor at vestibulum ornare, nisi lorem pharetra odio, in lacinia orci mi non odio. Vestibulum quis vulputate augue, finibus elementum odio. Vestibulum quis porttitor enim, nec dapibus risus. In hac habitasse platea dictumst.
-
-Nunc sollicitudin viverra se .
-.
-`;
